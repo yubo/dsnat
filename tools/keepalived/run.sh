d4c01bbf402f64cffd0dbcdf8291d718f568673e
yubo <yubo@xiaomi.com>
2014-11-18 12:06:40 +0800

    V2
    加入fullnat v2特性
    tools使用了netlink
    编译,调试通过,待测试

---
+++ tools/keepalived/run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#yum install libnl-devel
+#./configure --prefix=/ --mandir=/usr/local/share/man/ --with-kernel-dir=/usr/src/kernels/`uname  -r`
+#rpm -e libnl-devel
+#./configure --prefix=/ --mandir=/usr/local/share/man/ --with-kernel-dir=/root/git.n.xiaomi.com/linux 
+./configure --prefix=/ --mandir=/usr/local/share/man/ --with-kernel-dir=/root/work/linux-2.6.32-220.23.1.el6 --enable-debug
+#yum install libnl-devel
+#./configure --prefix=/ --mandir=/usr/local/share/man/ --with-kernel-dir=/usr/src/kernels/`uname  -r`
